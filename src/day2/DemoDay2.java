63088e9c05fb02cc42ff7308c586dc8ee9a7cf0e
stephaniemarrero <stephaniemarrero@localhost>
2023-07-29 22:49:47 -0400

    implemented subtract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,6 +5,23 @@ public class DemoDay2 {
 	public static void main (String arsg []) {
 		
 		System.out.println("Test");
+	
+	}
+	
+	//add method
+	public static void add(int a, int b) {
+			
+		System.out.println(a + b);
 		
 	}
+	
+	
+	//subtract
+	public static void subtract(int a, int b) {
+		
+		System.out.println(a - b);
+		
+	}
+	
+	
 }
